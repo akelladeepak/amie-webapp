d4036d8306473dfbd82cb465a7cdd908468fc02e
Deepak Akella <akelladeepak@gmail.com>
2025-03-14 15:31:04 -0400

    Use HashRouter instead of browser

--- amie/src/App.jsx
+++ amie/src/App.jsx
@@ -1,6 +1,6 @@
 // App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import pages
 import Login from './pages/Login';
