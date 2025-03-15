be94dd7226a2b5519aa4af7b9bb32c127763c544
edrikreyes <edrik.reyes9@gmail.com>
2025-03-15 12:51:54 -0400

    Style login page

--- amie/src/pages/Login.jsx
+++ amie/src/pages/Login.jsx
@@ -13,9 +13,9 @@ function Login() {
 
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
-      <div className="max-w-5xl w-full flex flex-col md:flex-row items-center p-6 md:space-x-40 space-y-8 md:space-y-0">
+      <div className="max-w-6xl w-full flex flex-col md:flex-row items-center p-6 md:space-x-6 space-y-8 md:space-y-0">
         {/* Left side text */}
-        <div className="w-full md:w-2/3 text-center">
+        <div className="w-full md:w-2/3">
           <h1 className="text-4xl md:text-6xl font-bold mb-4 text-gray-800">
             Welcome Back
           </h1>
@@ -24,41 +24,57 @@ function Login() {
           </p>
         </div>
         {/* Right side login form */}
-        <div className="w-full md:w-1/2 glassmorphic shadow-lg rounded-lg p-6 md:p-8">
-          <h2 className="text-xl md:text-2xl font-semibold text-gray-700 mb-6 md:mb-12">
+        <div className="w-full md:w-1/2 bg-white shadow-lg rounded-2xl px-8 pb-8 pt-4">
+          <div className='mb-6 md:mb-12'>
+          <h1 className='logo text-center'>
+            Amie
+          </h1>
+          {/* <h2 className="text-xl md:text-xl font-semibold text-gray-500 text-center">
             Sign In
-          </h2>
-          <form onSubmit={handleSignIn} className="flex flex-col space-y-4">
+          </h2> */}
+          </div>
+          <form onSubmit={handleSignIn} className="flex flex-col">
+            <p className='text-sm mb-2 font-semibold'>
+              Email
+            </p>
             <input
               type="email"
               placeholder="Enter your email"
-              className="bg-white rounded-lg px-4 py-3 focus:outline-none"
+              className="bg-white rounded-xl px-4 py-3 focus:outline-none border-gray-200 border mb-8"
               required
             />
+            <p className='text-sm mb-2 font-semibold'>
+              Password
+            </p>
             <input
               type="password"
               placeholder="Enter your password"
-              className="bg-white rounded-lg px-4 py-3 focus:outline-none"
+              className="bg-white rounded-xl px-4 py-3 focus:outline-none border-gray-200 border"
               required
             />
             <div className="flex justify-between text-sm text-[#3D76D7]">
-              <button type="button" className="hover:underline cursor-pointer">
+              <button type="button" className="hover:underline cursor-pointer ml-auto mb-4 mt-2">
                 Forgot Password?
               </button>
             </div>
             <button
               type="submit"
-              className="bg-[#3D76D7] text-white py-2 rounded hover:bg-[#3d66d7] cursor-pointer"
+              className="bg-[#3D76D7] text-white py-2.5 rounded-xl hover:bg-[#3d66d7] cursor-pointer font-bold mb-4 text-sm"
             >
               Sign In
             </button>
             <button
               type="button"
-              className="bg-white text-[#3D76D7] py-2 rounded hover:bg-gray-100 cursor-pointer"
+              className="bg-white text-[#3D76D7] py-2.5 rounded-xl hover:bg-gray-100 cursor-pointer border-[#3d66d7] border font-bold text-sm"
             >
               <Link to="/coming-soon">Create Profile</Link>
             </button>
           </form>
+          <div className='mt-4 mb-4 text-center'>
+            <p className='text-xs text-gray-500 font-medium'>
+              By signing in, you agree to our <a className='text-blue-500 cursor-pointer'>Terms of Service</a> and <a className='text-blue-500 cursor-pointer'>Privacy Policy</a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
