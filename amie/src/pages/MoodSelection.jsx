3720123d472b32f464f437205265f665cdd1aef0
Deepak Akella <akelladeepak@gmail.com>
2025-03-15 12:02:17 -0400

    Add cursor-pointer to x in modal

--- amie/src/pages/MoodSelection.jsx
+++ amie/src/pages/MoodSelection.jsx
@@ -231,7 +231,7 @@ function MoodSelection({ moodLogs, setMoodLogs }) {
             </div>
             <button
               onClick={() => setShowSecondModal(false)}
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 cursor-pointer"
             >
               ✕
             </button>
